090fc8323adaf5031b25fe6dfa919014f843337b
Pete Davison <pd93.uk@outlook.com>
2021-04-05 22:56:53 +0100

    Added recipes to convert between XP ingots and blocks

---
+++ src/main/java/org/hackedtogether/xpadditions/data/ModRecipeProvider.java
@@ -0,0 +1,36 @@
+package org.hackedtogether.xpadditions.data;
+
+import net.minecraft.data.*;
+import net.minecraft.item.crafting.Ingredient;
+import net.minecraft.util.ResourceLocation;
+import org.hackedtogether.xpadditions.XPAdditions;
+import org.hackedtogether.xpadditions.setup.ModBlocks;
+import org.hackedtogether.xpadditions.setup.ModItems;
+
+import java.util.function.Consumer;
+
+public class ModRecipeProvider extends RecipeProvider {
+    public ModRecipeProvider(DataGenerator generatorIn) {
+        super(generatorIn);
+    }
+
+    @Override
+    protected void buildShapelessRecipes(Consumer<IFinishedRecipe> consumer) {
+        ShapelessRecipeBuilder.shapeless(ModItems.XP_INGOT.get(), 9)
+                .requires(ModBlocks.XP_BLOCK.get())
+                .unlockedBy("has_item", has(ModItems.XP_INGOT.get()))
+                .save(consumer);
+
+        ShapedRecipeBuilder.shaped(ModBlocks.XP_BLOCK.get())
+                .define('#', ModItems.XP_INGOT.get())
+                .pattern("###")
+                .pattern("###")
+                .pattern("###")
+                .unlockedBy("has_item", has(ModItems.XP_INGOT.get()))
+                .save(consumer);
+    }
+
+    private static ResourceLocation modId(String path) {
+        return new ResourceLocation(XPAdditions.MOD_ID, path);
+    }
+}
