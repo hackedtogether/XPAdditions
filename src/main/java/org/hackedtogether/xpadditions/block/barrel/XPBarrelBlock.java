f8ebdcccb5e31518035147573bc259295a10e497
Pete Davison <pd93.uk@outlook.com>
2021-04-06 04:46:59 +0100

    Added basic XP Barrel block

---
+++ src/main/java/org/hackedtogether/xpadditions/block/barrel/XPBarrelBlock.java
@@ -0,0 +1,9 @@
+package org.hackedtogether.xpadditions.block.barrel;
+
+import net.minecraft.block.Block;
+
+public class XPBarrelBlock extends Block {
+    public XPBarrelBlock(Properties properties) {
+        super(properties);
+    }
+}
--- src/main/java/org/hackedtogether/xpadditions/data/client/ModBlockStateProvider.java
+++ src/main/java/org/hackedtogether/xpadditions/data/client/ModBlockStateProvider.java
@@ -2,6 +2,7 @@ package org.hackedtogether.xpadditions.data.client;
 
 import net.minecraft.data.DataGenerator;
 import net.minecraftforge.client.model.generators.BlockStateProvider;
+import net.minecraftforge.client.model.generators.ConfiguredModel;
 import net.minecraftforge.common.data.ExistingFileHelper;
 import org.hackedtogether.xpadditions.XPAdditions;
 import org.hackedtogether.xpadditions.common.registries.ModBlocks;
@@ -14,5 +15,13 @@ public class ModBlockStateProvider extends BlockStateProvider {
     @Override
     protected void registerStatesAndModels() {
         simpleBlock(ModBlocks.XP_BLOCK.get());
+        getVariantBuilder(ModBlocks.XP_BARREL.get()).forAllStates(state -> {
+            return ConfiguredModel.builder()
+                    .modelFile(models().cubeBottomTop("xp_barrel",
+                            modLoc("block/xp_barrel_side"),
+                            modLoc("block/xp_barrel_bottom"),
+                            modLoc("block/xp_barrel_top")))
+                    .build();
+        });
     }
 }
