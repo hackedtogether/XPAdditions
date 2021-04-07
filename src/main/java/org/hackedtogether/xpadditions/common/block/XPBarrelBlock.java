a1d37bd99ca38f961eec71da6943437120e6b755
Pete Davison <pd93.uk@outlook.com>
2021-04-07 20:00:38 +0100

    Allow XPBarrel to store and drop XP

--- src/main/java/org/hackedtogether/xpadditions/common/block/XPBarrelBlock.java
+++ src/main/java/org/hackedtogether/xpadditions/common/block/XPBarrelBlock.java
@@ -2,11 +2,22 @@ package org.hackedtogether.xpadditions.common.block;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.ActionResultType;
+import net.minecraft.util.Hand;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.BlockRayTraceResult;
 import net.minecraft.world.IBlockReader;
+import net.minecraft.world.IWorldReader;
+import net.minecraft.world.World;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.hackedtogether.xpadditions.common.tileentity.XPBarrelTileEntity;
 
 public class XPBarrelBlock extends Block {
+
+    private static final Logger LOGGER = LogManager.getLogger();
 
     public XPBarrelBlock(Properties properties) {
         super(properties);
@@ -20,5 +31,48 @@ public class XPBarrelBlock extends Block {
     @Override
     public TileEntity createTileEntity(BlockState state, IBlockReader world) {
         return new XPBarrelTileEntity();
+    }
+
+    @Override
+    public int getExpDrop(BlockState state, IWorldReader world, BlockPos pos, int fortune, int silktouch) {
+        XPBarrelTileEntity te = (XPBarrelTileEntity) world.getBlockEntity(pos);
+        return te.getStoredXP();
+    }
+
+    @Override
+    public ActionResultType use(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockRayTraceResult hit) {
+
+        if (world.isClientSide()) {
+            return ActionResultType.SUCCESS;
+        }
+
+        int playerXP = player.totalExperience;
+        int playerLevel = player.experienceLevel;
+        float playerLevelProgress = player.experienceProgress;
+
+        LOGGER.debug("PlayerEntity: '" + player.getUUID() + "' using barrel");
+        LOGGER.debug("before player xp level: " + playerLevel);
+        LOGGER.debug("before player xp total: " + playerXP);
+
+        if (playerXP > 0) {
+
+            // Fetch the tile entity
+            XPBarrelTileEntity te = (XPBarrelTileEntity) world.getBlockEntity(pos);
+            LOGGER.debug("before barrel xp: " + te.getStoredXP());
+            LOGGER.debug("xp progress: " + playerLevelProgress);
+
+            // Should we move all XP or just 1 level
+            if (player.isCrouching()) {
+                te.transferAllXPFromPlayer(player);
+            } else {
+                te.transferXPLevelFromPlayer(player);
+            }
+
+            LOGGER.debug("after player xp level: " + player.experienceLevel);
+            LOGGER.debug("after player xp total: " + player.totalExperience);
+            LOGGER.debug("after barrel xp: " + te.getStoredXP());
+        }
+
+        return super.use(state, world, pos, player, hand, hit);
     }
 }
--- src/main/java/org/hackedtogether/xpadditions/common/tileentity/XPBarrelTileEntity.java
+++ src/main/java/org/hackedtogether/xpadditions/common/tileentity/XPBarrelTileEntity.java
@@ -1,11 +1,17 @@
 package org.hackedtogether.xpadditions.common.tileentity;
 
-import net.minecraft.block.Blocks;
+import net.minecraft.block.BlockState;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.nbt.CompoundNBT;
 import net.minecraft.tileentity.ITickableTileEntity;
 import net.minecraft.tileentity.TileEntity;
 import org.hackedtogether.xpadditions.common.registries.ModTileEntityTypes;
+import org.hackedtogether.xpadditions.util.XPUtils;
 
 public class XPBarrelTileEntity extends TileEntity implements ITickableTileEntity {
+
+    protected int storedXP;
+    private static int maxStoredXP = 1395;
 
     public XPBarrelTileEntity() {
         super(ModTileEntityTypes.XP_BARREL.get());
@@ -14,6 +20,63 @@ public class XPBarrelTileEntity extends TileEntity implements ITickableTileEntit
     @Override
     public void tick() {
         // Test code that will delete the block under the barrel.
-        this.level.setBlockAndUpdate(this.worldPosition.below(), Blocks.AIR.defaultBlockState());
+        // this.level.setBlockAndUpdate(this.worldPosition.below(), Blocks.AIR.defaultBlockState());
+    }
+
+    @Override
+    public CompoundNBT save(CompoundNBT nbt) {
+        super.save(nbt);
+        nbt.putInt("xp", this.storedXP);
+        return nbt;
+    }
+
+    @Override
+    public void load(BlockState state, CompoundNBT nbt) {
+        super.load(state, nbt);
+        this.storedXP = nbt.getInt("xp");
+    }
+
+    public int getStoredXP() {
+        return this.storedXP;
+    }
+
+    public void addXP(int xp) {
+        xp = this.storedXP + xp;
+        if (xp < 0) {
+            throw new NumberFormatException("Stored XP cannot be negative");
+        }
+        this.storedXP = xp;
+    }
+
+    public int getRemainingSpace() {
+        return this.maxStoredXP - this.storedXP;
+    }
+
+    public int getMaxTransferableXP(PlayerEntity player) {
+        return Math.min(this.getRemainingSpace(), player.totalExperience);
+    }
+
+    public void transferXPLevelFromPlayer(PlayerEntity player) {
+
+        int xpForCurrentLevel = XPUtils.getXPForLevel(player.experienceLevel);
+        int xpToMove = XPUtils.getPlayerXP(player) - xpForCurrentLevel;
+
+        // Player has exactly x > 0 levels (xp bar looks empty)
+        if (xpToMove == 0 && player.experienceLevel > 0) {
+            xpToMove = xpForCurrentLevel - XPUtils.getXPForLevel(player.experienceLevel - 1);
+        }
+
+        transferXPFromPlayer(player, xpToMove);
+    }
+
+    public void transferAllXPFromPlayer(PlayerEntity player) {
+        transferXPFromPlayer(player, this.getMaxTransferableXP(player));
+    }
+
+    public void transferXPFromPlayer(PlayerEntity player, int xp) {
+        if (xp > 0) {
+            player.giveExperiencePoints(-xp);
+            this.addXP(xp);
+        }
     }
 }
