bbe0b67fb8a30e3dbd9d051dd7571e4d47fe8c15
Pete Davison <pd93.uk@outlook.com>
2021-04-11 23:29:24 +0100

    Firework max stack to 1

--- src/main/java/org/hackedtogether/xpadditions/common/registries/ModItems.java
+++ src/main/java/org/hackedtogether/xpadditions/common/registries/ModItems.java
@@ -8,7 +8,7 @@ import org.hackedtogether.xpadditions.common.registration.ModItemRegister;
 
 public class ModItems {
     public static final RegistryObject<Item> XP_INGOT = ModItemRegister.register("xp_ingot", () -> new Item(new Item.Properties().tab(ItemGroup.TAB_MATERIALS)));
-    public static final RegistryObject<Item> XP_FIREWORK_ROCKET = ModItemRegister.register("xp_firework_rocket", () -> new XPFireworkRocketItem(new Item.Properties().tab(ItemGroup.TAB_MISC)));
+    public static final RegistryObject<Item> XP_FIREWORK_ROCKET = ModItemRegister.register("xp_firework_rocket", () -> new XPFireworkRocketItem(new Item.Properties().tab(ItemGroup.TAB_MISC).stacksTo(1)));
 
     public static void register() {
     }
